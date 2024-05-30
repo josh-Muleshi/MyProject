d59ae25e760ca4f52ee1349fed521d54d116e738
josh muleshi <jmuleshi2@gmail.com>
2024-05-30 05:26:33 -0400

    test slp_object_id

--- courses/poo.php
+++ courses/poo.php
@@ -1,2 +1,16 @@
 <?php
 
+class Person {
+    public int $id;
+}
+
+$a = new Person();
+$b = $a;
+$a->id = 20;
+
+echo spl_object_id($a);
+echo "\n";
+echo spl_object_id($b);
+echo "\n";
+echo $b->id;
+echo "\n";
